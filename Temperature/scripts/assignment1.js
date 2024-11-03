cea98bd04ba60b806feec3a2fe9b69d9385a1453
Joseatorres513 <joseatorres513@gmail.com>
2024-11-03 11:21:44 -0500

    Add files via upload

---
+++ Temperature/scripts/assignment1.js
@@ -0,0 +1,31 @@
+console.log("Script");
+// 1. Declare Variables
+    // Using let
+    let character = "Daisy";
+    let action = "stay";
+    let object = "a well fed beagle";
+    let place = "bakery";
+    let time = "a perfect Sunday";
+
+    // Using "const"
+    const occasion = "a puppy treat bakery";
+
+    // 2. Create the Story
+    console.log ("--- Funny Scene ---");
+    console.log(character + " was at " + place + " on " + time + ".");
+    console.log("When walking, " + character + " caught a glimpse of " + object + " and was happy to " + action + ".")
+    console.log("Looking around she realized it was " + occasion + " and there were puppies everywhere!");
+
+    // 3. Experiment with Different Values
+    console.log("--- Different Version of the Story");
+
+    // Change values to create a different story
+    character = "Julia";
+    action = "run";
+    object = "a really big oak tree";
+    place = "a dog park";
+    time = "a busy Saturday";
+
+    console.log(character + " was at " + place + "on" + time + ".");
+    console.log("Effortlessly " + character + " played with many dogs near " + object + " and wanted to make them " + action + ".");
+    console.log("All the dogs were puppies just coming from " + occasion + " and everyone took a nap afterwards!");
